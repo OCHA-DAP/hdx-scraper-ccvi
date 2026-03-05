eb25135b6585225f733cfb832df70949d526b119
mcarans <rans@email.com>
2026-03-05 17:44:05 +1300

    Remove hxl

--- src/hdx/scraper/ccvi/__main__.py
+++ src/hdx/scraper/ccvi/__main__.py
@@ -70,7 +70,6 @@ def main(
                 dataset.create_in_hdx(
                     remove_additional_resources=True,
                     match_resource_order=True,
-                    hxl_update=False,
                     updated_by_script=_UPDATED_BY_SCRIPT,
                     batch=info["batch"],
                 )
